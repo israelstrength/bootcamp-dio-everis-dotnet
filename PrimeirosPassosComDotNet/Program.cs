e446872cfc1804df4b5b07eca55ff031a47676a4
israelforce <Israelmtb2012@gmail.com>
2021-09-22 23:09:36 -0300

    update

--- PrimeirosPassosComDotNet/Program.cs
+++ PrimeirosPassosComDotNet/Program.cs
@@ -6,8 +6,8 @@ namespace PrimeirosPassosComDotNet
     {
         static void Main(string[] args)
         {
-            int numeroDeVezes = 10;
-            for(int i = 0; i < numeroDeVezes; i++)
+            int numeroDeVezes = 20;
+            for(int i = 1; i < numeroDeVezes; i++)
             {
                 Console.WriteLine($"Bem-vindo ao curso de .NET {i}");
             }
