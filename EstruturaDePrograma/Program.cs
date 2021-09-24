35fcf22b532f21f277cf24ffd00dcb17b4485a3f
israelforce <Israelmtb2012@gmail.com>
2021-09-24 02:31:29 -0300

    Curso Estrutura de Programa

---
+++ EstruturaDePrograma/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using EstruturaDePrograma.Exemplos;
+
+namespace EstruturaDePrograma
+{
+    class Program
+    {
+        static void Main()
+        {
+            var a = new Pilha();
+            a.Empilha(1);
+            a.Empilha(10);
+            a.Empilha(100);
+            Console.WriteLine(a.Desempilha());
+            Console.WriteLine(a.Desempilha());
+            Console.WriteLine(a.Desempilha());
+        }
+    }
+}
