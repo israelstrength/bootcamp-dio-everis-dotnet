d613fa2b8ca4976b2f8a99c523020205afb7d593
israelforce <Israelmtb2012@gmail.com>
2021-09-27 02:20:05 -0300

    Revisão do projeto

---
+++ Revisão/Revisão/Aluno.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revisão
+{
+    public struct Aluno
+    {
+
+        public string Nome { get; set; }
+        public decimal Nota { get; set; }
+    }
+}
